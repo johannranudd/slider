916da3be8660101b7654d0605ec79e5df2a5055e
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-08-13 12:37:19 +0200

    zdth

--- js/script.js
+++ js/script.js
@@ -1,43 +1,8 @@
 const slider = document.querySelectorAll(".slider");
 const prevBtn = document.querySelector(".prevBtn");
 const nextBtn = document.querySelector(".nextBtn");
-const profile = document.querySelectorAll(".profile h3");
-
-profile.forEach(item => {
-    item.style.marginTop = '5rem';
-})
 
 let counter = 0;
-
-// function slideFunction(item, index) {
-//     item.forEach(slideItem => {
-//         slideItem.style.transform = `translate(-${index * 100}%)`
-//     })
-// }
-
-// nextBtn.addEventListener("click",() => {
-//     counter++;
-//     slideFunction(slider, counter);
-// })
-// prevBtn.addEventListener("click",() => {
-//     counter--;
-//     slideFunction(slider, counter);
-// })
-
-window.addEventListener("DOMContentLoaded", onLoadFn);
-
-function onLoadFn() {
-    let hundred = -100;
-    for (let i = 0; i < slider.length; i++) {
-        hundred += 100;
-        slider[i].style.left = `${hundred}%`;
-        
-
-    }
-    slider.style.left = `-100%`;
-    // slider[0].style.left = `${hundred}%`;
-}
-
 
 function slideFunction(item, index) {
   item.forEach((slideItem) => {
@@ -51,6 +16,7 @@ nextBtn.addEventListener("click", () => {
     counter = 0;
   }
   slideFunction(slider, counter);
+  console.log(counter);
 });
 prevBtn.addEventListener("click", () => {
   counter--;
@@ -59,6 +25,3 @@ prevBtn.addEventListener("click", () => {
   }
   slideFunction(slider, counter);
 });
-
-// if (counter >)
-// console.log(slider.length);
